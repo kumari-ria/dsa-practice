2abc34811ea9b5e63fe6e1743643edd7e3fa8b4a
RIYA KUMARI <riyakumarisharma9@gmail.com>
2025-08-31 22:19:00 +0530

    Add files via upload

---
+++ tree.java
@@ -0,0 +1,42 @@
+public class tree {
+    static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+        System.out.println(root.left.data);
+        System.out.println(root.right.data);
+        System.out.println(root.left.left.data);
+        System.out.println(root.left.right.data);
+        System.out.println(root.right.right.data);
+    }
+
+}
